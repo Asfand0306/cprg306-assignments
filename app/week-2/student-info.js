66f784c21b4cf43d96a3b36347bf99a298672144
Asfand7867 <161638610+Asfand7867@users.noreply.github.com>
2025-01-19 17:11:48 -0700

    Week-2 Tasks Completed
    Cleaned files and Week 2 tasks Completed.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+        <p>Asfand Khan</p>
+        <Link href="https://github.com/Asfand0306">Github Page</Link>
+        </main>
+    );
+}
