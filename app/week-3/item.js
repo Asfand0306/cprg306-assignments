7681d3c9461b5b818a7ec2dcd791b18833aa4719
Asfand7867 <161638610+Asfand7867@users.noreply.github.com>
2025-01-28 19:02:47 -0700

    week-3 assignment added
    added app/week-3 folder and components.
    added shopping list, components and props.

---
+++ app/week-3/item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Item = ({ name, quantity, category }) => {
+    return (
+        <li className= "bg-zinc-700 hover:bg-zinc-500 p-6 my-5 rounded-lg w-96 ">
+            <div className="font-bold text-lg text-transform: capitalize">{name}</div>
+            <div className="text-emerald-500">Quantity: {quantity}</div>
+            <div className="text-emerald-500 text-transform: capitalize">Category: {category}</div>
+        </li>
+    );
+};
+
+export default Item;
---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import ItemList from './item-list.js';
+
+export default function Page() {
+    return (
+        <main className="p-4">
+            <h1 className="text-3xl font-bold text-green-300 ">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+}
