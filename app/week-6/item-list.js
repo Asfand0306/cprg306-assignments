25d652527a792635c84877cdeecf8c79145c2cad
Asfand0306 <166415318+Asfand0306@users.noreply.github.com>
2025-02-28 22:04:21 -0700

    week 6 fix

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -7,8 +7,32 @@ import items from './items.json';
 const ItemList = () => {
     const [sortBy, setSortBy] = useState("name");
     
+    const sortedItems = [...items].sort((a, b) => {
+        if (sortBy === "name") {
+            return a.name.localeCompare(b.name);
+        } else if (sortBy === "category") {
+            return a.category - b.category;
+        };
+    });
 
+    return (
+        <div>
+            <h1>Items</h1>
+            <label>
+                Sort by:
+                <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                    <option value="name">Name</option>
+                    <option value="category">Category</option>
+                </select>
+            </label>
+            <ul>
+                {sortedItems.map((item) => (
+                    <Item key={item.id} item={item} />
+                ))}
+            </ul>
+        </div>
+    );
 
-};
+}
 
 export default ItemList;
--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,10 +1,10 @@
-import itemList from './item-list.js';
+import ItemList from './item-list.js';
 
 export default function Page() {
     return (
         <main className="p-4">
             <h1 className="text-3xl font-bold text-green-300 ">Shopping List</h1>
-            <itemList />
+            <ItemList />
         </main>
     );
 }
